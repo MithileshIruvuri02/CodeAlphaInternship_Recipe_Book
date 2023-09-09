fb3472d489d997c458bcf441073f4e16d824cba7
Sujata <sujataprasad2004@gmail.com>
2023-09-09 14:46:22 +0530

    mongoose setup

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+
+// const mongoURI="mongodb://localhost:27017";
+
+const connectToMongo=async()=>{
+    mongoose.connect("mongodb://127.0.0.1:27017")
+    //   try{
+    //     console.log("Connected to mongo successfully");
+    //   }  catch(error){ console.log(error)}
+        .then((success)=>console.log('Connected successfully'))
+        .catch((err)=>console.log(err.message));
+    // })
+}
+
+module.exports=connectToMongo;
