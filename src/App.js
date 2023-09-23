af446f3959c0b10467fcdbdf8435a59eeaf54d28
Sujata <sujataprasad2004@gmail.com>
2023-09-23 17:00:52 +0530

    Adding Login component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 
 function App() {
@@ -25,6 +27,10 @@ function App() {
           <Route exact path="/home" element={<Home/>}>
           </Route>
           <Route exact path="/about" element={<About/>}>
+          </Route>
+          <Route exact path="/login" element={<Login/>}>
+          </Route>
+          <Route exact path="/signup" element={<Signup/>}>
           </Route>
         </Routes>
         </div>
