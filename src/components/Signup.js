895a46ee0c380fd766d9e52edbbab294d0e2e536
Sujata <sujataprasad2004@gmail.com>
2023-10-11 20:32:04 +0530

    changes in signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -37,7 +37,7 @@ const Signup = (props) => {
 
   return (
     <div className='container mt-2'>
-      <h2 className='my-3'>Craete an account to use iNotebook</h2>
+      <h2 className='my-3'>Create an account to use iNotebook</h2>
       <form onSubmit={handleSubmit}>
         <div className="my-3">
           <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
