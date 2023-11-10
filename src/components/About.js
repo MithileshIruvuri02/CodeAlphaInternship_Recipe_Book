c50ac9dc5257be8a391a88e094f99e8b00e33b26
sujataprasad01 <sujataprasad2004@gmail.com>
2023-11-10 14:03:30 +0530

    final commit

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
 
   return (
     <div className='container'>
-      <h6>It is a Note keeping app, where You can save your notes daily and make your busy schedule a little bit organize.</h6>
+      <h6>It is a Recipe keeping app, where You can save your fav recipes organize and add, edit and delete whenever you want.</h6>
       -- Created by <a href='https://www.linkedin.com/in/sujata-prasad'>Sujata Prasad</a>
     </div>
   )
